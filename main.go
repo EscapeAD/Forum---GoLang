d80c79bc3db231539243a8b7b1f4c9e79709bffe
Adam Tse <mr.adam.tse@gmail.com>
2017-03-14 07:44:18 -0400

    message interface added, api route, +notes

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 	"net/http"
 )
 
+type message struct {
+	User    string
+	Message string
+}
+
 func home(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Test")
+	// Set template here
+	io.WriteString(w, "Home")
+}
+func forum(w http.ResponseWriter, req *http.Request) {
+	// response json
+
 }
 
 func main() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/api/", forum)
 	http.ListenAndServe(":8080", nil)
 }
