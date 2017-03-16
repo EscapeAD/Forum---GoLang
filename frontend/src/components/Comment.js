096a6f84d93934332757d1da2c564b79bf0bc2fa
Adam Tse <mr.adam.tse@gmail.com>
2017-03-16 08:49:13 -0400

    - Pass postRespond function down to comment component

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Reply                from './Reply';
+import Respond              from './Respond';
+
 
 class Comment extends Component {
   render() {
     let comments = this.props.state.comments.map((comment)=>{
       if(this.props.message_id === comment.message_id){
-      return <li key={comment.id}>{comment.message} - <strong>{comment.username}</strong> - <i>{new Date(comment.created_at).toLocaleString()}</i> <Reply comment_id={comment.id} message_id={comment.message_id} state={this.props.state}></Reply></li>
+      return <li key={comment.id}>{comment.message} - <strong>{comment.username}</strong> - <i>{new Date(comment.created_at).toLocaleString()}</i> - <Respond postResponse={this.props.postResponse.bind(this)} comment_id={comment.id} message_id={comment.message_id}></Respond>  <Reply comment_id={comment.id} message_id={comment.message_id} state={this.props.state}></Reply></li>
       }
     })
     return (
