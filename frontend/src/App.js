f95bf31ca2a1048946b11bb295329aa75dddc1e3
Adam Tse <mr.adam.tse@gmail.com>
2017-03-16 04:41:48 -0400

    - Added fontawesome CDN - created Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <Navbar></Navbar>
       </div>
     );
   }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   render() {
     return (
-      <nav>
-
+      <nav className="navbar navbar-inverse bg-inverse">
+        <div className="container">
+        <a className="navbar-brand" href="#">
+        <i className="fa fa-globe" aria-hidden="true"></i> NAV-eddit
+        </a>
+        </div>
       </nav>
     );
   }
