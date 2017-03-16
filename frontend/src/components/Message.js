7f9aa7f968e770d02985886e94d4237b67617a3b
Adam Tse <mr.adam.tse@gmail.com>
2017-03-16 08:10:07 -0400

    - message_id pass to Respond

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -5,7 +5,7 @@ import Respond              from './Respond';
 class Message extends Component {
   render() {
     let message = this.props.state.messages.map((msg)=>{
-      return <li key={msg.id}>{msg.message} - <strong>{msg.username}</strong> - <i>{new Date(msg.created_at).toLocaleString()}</i> - <Respond></Respond> <Comment message_id={msg.id} state={this.props.state}></Comment></li>
+      return <li key={msg.id}>{msg.message} - <strong>{msg.username}</strong> - <i>{new Date(msg.created_at).toLocaleString()}</i> - <Respond message_id={msg.id}></Respond> <Comment message_id={msg.id} state={this.props.state}></Comment></li>
     })
     return (
       <div>
