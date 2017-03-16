575dffff20c65259d31d18ed48e4299f7df544bf
Adam Tse <mr.adam.tse@gmail.com>
2017-03-16 07:56:04 -0400

    - Respond Modal created, handleSubmit() connected - import Respond component into Message Component

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Comment              from './Comment';
+import Respond              from './Respond';
 
 class Message extends Component {
   render() {
     let message = this.props.state.messages.map((msg)=>{
-      return <li key={msg.id}>{msg.message} - <strong>{msg.username}</strong> - <i>{new Date(msg.created_at).toLocaleString()}</i><Comment message_id={msg.id} state={this.props.state}></Comment></li>
+      return <li key={msg.id}>{msg.message} - <strong>{msg.username}</strong> - <i>{new Date(msg.created_at).toLocaleString()}</i> - <Respond></Respond> <Comment message_id={msg.id} state={this.props.state}></Comment></li>
     })
     return (
       <div>
--- frontend/src/components/Respond.js
+++ frontend/src/components/Respond.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Respond extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    modal: false
+  };
+  this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+  checkInfo(){
+
+  }
+  handleSubmit(event){
+    event.preventDefault()
+    console.log('Connected')
+  }
+
   render() {
     return (
-        <div>
-        </div>
+      <span>
+        <Button color="danger" onClick={this.toggle}>Reply</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+        <Form onSubmit={this.handleSubmit.bind(this)}>
+          <ModalBody>
+                 <FormGroup>
+                   <Label for="username">Username</Label>
+                   <Input type="text" name='username' id="username" placeholder="John Doe" required/>
+                 </FormGroup>
+                 <FormGroup>
+                   <Label for="message">Post</Label>
+                   <Input type="textarea" name='messsage' id="message" required />
+                 </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <input type='submit' className='btn btn-success' value='submit' />
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+          </Form>
+        </Modal>
+      </span>
     );
   }
 }
