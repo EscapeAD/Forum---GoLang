2e93bdeb87131b41db373e80f5683109a33b3684
Adam Tse <mr.adam.tse@gmail.com>
2017-03-16 10:40:54 -0400

    - Match reply component to blue theme

--- frontend/src/components/Reply.js
+++ frontend/src/components/Reply.js
@@ -4,7 +4,8 @@ class Reply extends Component {
   render() {
     let replies = this.props.state.replies.map((reply)=>{
       if(this.props.comment_id === reply.comment_id && this.props.message_id === reply.message_id){
-      return <li key={reply.id}>{reply.message} - <strong>{reply.username}</strong> - <i>{new Date(reply.created_at).toLocaleString()}</i></li>
+      return <li key={reply.id}><div className="message-box">{reply.message}</div><div className="message-footer bg-primary"><span className="user-details">Posted By: <strong>{reply.username}</strong> - <i>{new Date(reply.created_at).toLocaleString()}</i></span></div></li>
+
     }
     })
     return (
