b87cdff20fd1d712ac4f96a7f7970857b4698ef5
Adam Tse <mr.adam.tse@gmail.com>
2017-03-16 06:03:24 -0400

    - Bootstrap form generated

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -4,10 +4,17 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 class Post extends Component {
   render() {
     return (
-      <form>
-
-
-      </form>
+      <Form className='NewPost'>
+             <FormGroup>
+               <Label for="username">Username</Label>
+               <Input type="text" name='username' id="username" placeholder="John Doe" required/>
+             </FormGroup>
+             <FormGroup>
+               <Label for="message">Post</Label>
+               <Input type="textarea" name='messsage' id="message" required />
+             </FormGroup>
+             <input type='submit' className='btn btn-success' value='submit' />
+      </Form>
     );
   }
 }
